14d7205bd679ecee9f6b7b5a5647f54da46ded28
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2024-03-05 16:46:31 +0100

    Revert "Transition to Electron"
    This reverts commit cc324d4a071e7d13c8f8def5282f864af80c722e.

--- build/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-contextBridge.exposeInMainWorld("electronAPI", {
-  onNewGame(callback) {
-    return ipcRenderer.on("new-game", (event, value) => callback(value));
-  }
-});
